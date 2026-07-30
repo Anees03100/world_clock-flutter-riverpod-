790be7ce73766a6ae1314750f5dce67b022cbc18
anees03100@gmail.com <anees03100@gmail.com>
2026-07-30 16:36:11 +0500

    dynamic time change

--- lib/widgets/hero_section.dart
+++ lib/widgets/hero_section.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:modern_world_clock/providers/locations_provider.dart';
+import 'package:modern_world_clock/providers/toggle_colors.dart';
 
-class HeroSection extends StatelessWidget {
+class HeroSection extends ConsumerWidget {
   const HeroSection({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locations = ref.watch(locationProvider);
+    final is24Hour = ref.watch(toggleColorProvider); // false = 12H, true = 24H
+    if (locations.isEmpty || locations.first.currentDateTime == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    final DateTime now = locations.first.currentDateTime!;
+
+    int hourInt = now.hour;
+    if (!is24Hour) {
+      hourInt = hourInt > 12 ? hourInt - 12 : hourInt;
+      if (hourInt == 0) hourInt = 12;
+    }
+    String hour = hourInt < 10 ? '0$hourInt' : '$hourInt';
+
+    String minute = now.minute < 10 ? '0${now.minute}' : '${now.minute}';
+    String second = now.second < 10 ? '0${now.second}' : '${now.second}';
+
+    const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+
+    String weekday = weekdays[now.weekday - 1];
+    String date = '${now.day} ${months[now.month - 1]}';
+
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -12,11 +52,11 @@ class HeroSection extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              '08',
+              hour,
               style: TextStyle(fontSize: 118, fontWeight: FontWeight.bold),
             ),
             Text(
-              '40',
+              minute,
               style: TextStyle(
                 fontSize: 118,
                 fontWeight: FontWeight.bold,
@@ -31,11 +71,11 @@ class HeroSection extends StatelessWidget {
 
           children: [
             Text(
-              'Thu',
+              weekday,
               style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             ),
             Text(
-              '30 July',
+              date,
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
@@ -48,7 +88,7 @@ class HeroSection extends StatelessWidget {
               ),
               child: Center(
                 child: Text(
-                  '15',
+                  second,
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
               ),
